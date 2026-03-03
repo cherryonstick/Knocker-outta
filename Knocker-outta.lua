3eb7c7c749aa74daa851a143ffa90460acbadaf5
cherryonstick <cherrytoeeater@gmail.com>
2026-03-02 23:04:59 -0500

    knocker-Outta-script
    uh the real thing

---
+++ Knocker-outta.lua
@@ -0,0 +1,68 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Knocker outta",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Loading the knocker, yeah.",
+   LoadingSubtitle = "By @jeffbob900 on discord",
+   ShowText = "Show gui", -- for mobile users to unhide Rayfield, change if you'd like
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false -- Prevents Rayfield from emitting warnings when the script has a version mismatch with the interface.
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = A real knocker outta folder, -- Create a custom folder for your hub/game
+      FileName = "Knocker outta file"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include Discord.gg/. E.g. Discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the Discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique, as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that the system will accept, can be RAW file links (pastebin, github, etc.) or simple strings ("hello", "key22")
+   }
+})
+
+local Tab = Window:CreateTab("Movement", "move")
+
+
+Section:Set("WalkSpeed Changer")
+
+local SpeedSlider = MainTab:CreateSlider({
+   Name = "Player WalkSpeed",
+   Range = {16, 200},       -- From default Roblox value up to quite fast but not insane
+   Increment = 1,           -- Smooth 1-stud steps
+   Suffix = "Studs/sec",
+   CurrentValue = 16,       -- Starts at normal speed
+   Flag = "WalkSpeedValueKnockeroutta", -- Unique identifier (required if saving, but harmless)
+   Callback = function(Value)
+      -- This runs every time the slider moves
+      local player = game.Players.LocalPlayer
+      if player and player.Character then
+         local humanoid = player.Character:FindFirstChildOfClass("Humanoid")
+         if humanoid then
+            humanoid.WalkSpeed = Value
+         })
+            
+
+
+
+
+
+
+
+Rayfield:LoadConfiguration()
